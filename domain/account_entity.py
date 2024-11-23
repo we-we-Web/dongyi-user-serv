c1f8277d7c1014a513fbd6aef55089c6fa17ef64
Ateto1202 <tonysu1204@gmail.com>
2024-11-23 13:16:26 +0800

    Set cart props as nullable

--- domain/account_entity.py
+++ domain/account_entity.py
@@ -4,7 +4,7 @@ from typing import List, Optional
 class AccountEntity(BaseModel):
     id: str # email
     name: str
-    cart: str
+    cart: Optional[str] = None
     orders: Optional[List[str]] = []
     created_at: Optional[str] = None
     updated_at: Optional[str] = None
